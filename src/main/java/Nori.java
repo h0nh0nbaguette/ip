3ee4ed397f7660ccdef84791b3e64b70715242ab
h0nh0nbaguette <noeltong245@gmail.com>
2026-08-19 19:36:34 +0800

    Echo user commands until exit

--- src/main/java/Nori.java
+++ src/main/java/Nori.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Entry point for Nori, a command-line task assistant.
  */
@@ -10,16 +12,27 @@ public class Nori {
             + "|_| \\_|\\___/|_|  |_|\n";
 
     /**
-     * Greets the user and exits.
+     * Greets the user, echoes commands, and exits when the user enters {@code bye}.
      *
      * @param args command-line arguments, which are not used
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(DIVIDER);
         System.out.println(BANNER);
         System.out.println("Hello! I'm Nori.");
         System.out.println("What can I do for you?");
         System.out.println(DIVIDER);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(command);
+            System.out.println(DIVIDER);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(DIVIDER);
     }
